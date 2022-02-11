2a70482181c70d8f09379710cc39d89802f09dfe
khkim <cdffee1@ntglobal.com>
2022-02-11 14:18:28 +0900

    class

--- p_test/test_01.py
+++ p_test/test_01.py
@@ -2,13 +2,8 @@
 
 class arr:
     a = ["Seoul", "Kyeonggi", "Inchon", "Daejoen", "Deagu", "Pusan"]
-    str01=' '
-    
-    def showMsg(self): #생성자
-        print(self.str01)
-
+    str01 = ' '
     for i in a:
         str01 = str01 + i[0]
 
-print_on = arr()
-print_on.showMsg()
+print(arr.str01)
