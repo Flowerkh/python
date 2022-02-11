5f4a556fbe1451d44d9c26b262bb15f7d49247a0
khkim <cdffee1@ntglobal.com>
2022-02-11 17:11:18 +0900

    암호 풀기 수정

--- lockopen_password.py
+++ lockopen_password.py
@@ -1,14 +1,14 @@
-lock = "LOCKED 암호를 입력해주세요"
 unlock = "Welcome"
 wrong_password = "LOCKED 잘못된 암호입니다."
-
 password = "A1234!"
 
 user_input = input("!!잠금!! 비밀번호를 입력해주세요")
 
-if user_input == password :
-    print("잠금이 해제되었습니다.")
-    print(unlock)
-else :
-    print("암호를 잘못 입력하였습니다.")
+while user_input != 'A1234!':
     print(wrong_password)
+    user_input = input("잘못된 비밀번호 입니다. 다시 입력해주세요.")
+
+print(unlock)
+print("잠금이 해제되었습니다.")
+
+
