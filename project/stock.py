c54ece9c2b01aae68fa8196c3b3cc30c4962dd16
khkim <cdffee1@ntglobal.com>
2022-05-30 17:58:11 +0900

    주식 금액수정

--- project/stock.py
+++ project/stock.py
@@ -1,33 +1,36 @@
 from foreign_trade import *
 from sync_API import *
 import requests
-import time
 
 def main():
+    path = "./token.txt"
+
     try:
-        #ACCESS_TOKEN = token()
-        #print(ACCESS_TOKEN)
-        ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjI1OWZhMzYzLTE3YzgtNDBmZS05YWIyLWM5Y2U2NmM5NzdmYSIsImlzcyI6InVub2d3IiwiZXhwIjoxNjUwOTU5MTQyLCJpYXQiOjE2NTA4NzI3NDIsImp0aSI6IlBTQktscW9ZNXZTYWxmbVQ3RFZkZEwwOWcyZHh2cXI2cWVPUiJ9.DvEwdez2mjTkO3rah1-E4YTGpXgM0mIm3SSt3zI82NM9XNfxN-zOq3TfMu2vlYKK8z2kMp2er0kNDqB1mdI8wA'
+        f = open(path, 'r', encoding='utf-8')
+        line = f.readline()
+        ACCESS_TOKEN = line
+
         for arr in info(ACCESS_TOKEN,'NASD'):
-            time.sleep(1)
             if arr['ovrs_excg_cd']=='NASD': excd='NAS'
             if arr['ovrs_excg_cd']=='NYSE': excd='NYS'
-            s_price = float(arr['pchs_avg_pric'])-10
-            h_price = float(arr['pchs_avg_pric'])+3
-            avr_price = round((float(arr['now_pric2'])-float(arr['pchs_avg_pric'])),2)
-            if(s_price<=float(arr['now_pric2'])<=h_price):
-                pass
-                #trade_val = trade(ACCESS_TOKEN, arr['ovrs_excg_cd'], arr['ovrs_pdno'], arr['now_pric2'])
-                #print(trade_val['msg1'])
-            print(search(ACCESS_TOKEN,excd,arr['ovrs_pdno']))
-            print(f"{arr['ovrs_pdno']}({arr['ovrs_excg_cd']}), 평균가 : {arr['pchs_avg_pric']}, 현재가 : {arr['now_pric2']}, 차액(현재가-평균가) : {round((float(arr['now_pric2'])-float(arr['pchs_avg_pric'])),2)}, 보유수량 : {int(arr['ovrs_cblc_qty'])}")
+            price = float(arr['now_pric2']) #현재가
+
+            if(arr['ovrs_pdno']=='AAPL'):
+                if(140<price<=200):
+                    print(f"{arr['ovrs_pdno']}({arr['ovrs_excg_cd']}), 평균가 : {arr['pchs_avg_pric']}, 현재가 : {arr['now_pric2']}, 차액(현재가-평균가) : {round((float(arr['now_pric2'])-float(arr['pchs_avg_pric'])),2)}, 보유수량 : {int(arr['ovrs_cblc_qty'])}")
+                    #trade_val = trade(ACCESS_TOKEN, arr['ovrs_excg_cd'], arr['ovrs_pdno'], arr['now_pric2'])
+                    #print(trade_val['msg1'])
 
     except Exception as e:
         print(e)
+        f = open(path, 'w', encoding='utf-8')
+        ACCESS_TOKEN = token()
+        f.write(ACCESS_TOKEN)
 
 def token():
     URL = f"{Sync_API.URL_BASE}{Sync_API.PATH}"
     res = requests.post(URL, headers=Sync_API.headers, data=json.dumps(Sync_API.body))
+    print(res.json())
     token = res.json()['access_token']
 
     return token
