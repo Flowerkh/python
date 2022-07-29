a6a4dd43f3050f0c7c1f7ec1fc29727203b66741
khim <cdffee1@naver.com>
2022-07-29 12:02:18 +0900

    주식자동매매 update

--- project/stock_macro/stock.py
+++ project/stock_macro/stock.py
@@ -5,14 +5,16 @@ import json
 from datetime import datetime
 
 time = datetime.now()
+now = datetime.today().strftime("%Y%m%d")
 
 def main():
-    #f = open("/home/project/stock/token.txt", 'r', encoding='utf-8')
-    #w = open("/home/project/stock/log/cron_log.txt", 'a', encoding='utf-8')
-    f = open("./token.txt", 'r', encoding='utf-8')
-    w = open("./log/cron_log.txt", 'a', encoding='utf-8')
-    #with open("/home/project/stock/kakao_code.json", "r") as kakao:
-    with open("kakao_code.json", "r") as kakao:
+    f = open("/home/project/stock/token.txt", 'r', encoding='utf-8')
+    w = open("/home/project/stock/log/cron_log"+now+".txt", 'a', encoding='utf-8')
+    #f = open("./token.txt", 'r', encoding='utf-8')
+    #w = open("./log/cron_log.txt", 'a', encoding='utf-8')
+    #with open("kakao_code.json", "r") as kakao:
+
+    with open("/home/project/stock/kakao_code.json", "r") as kakao:
         kaka_tks = json.load(kakao)
 
     kakao_url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
@@ -33,28 +35,27 @@ def main():
             kor = price*dollor
 
             if(arr['ovrs_pdno']=='AAPL'):
-                if(price<=180):
-                    if(kor<=200000):
-                        w.write(f"({time.strftime('%Y-%m-%d %H:%M:%S')}) {arr['ovrs_pdno']}({arr['ovrs_excg_cd']}), 현재가 : {arr['now_pric2']}, 평균가 : {arr['pchs_avg_pric']}, 차액(현재가-평균가) : {round((float(arr['now_pric2']) - float(arr['pchs_avg_pric'])), 2)}, 보유수량 : {int(arr['ovrs_cblc_qty'])}, KOR : {round(kor, 2)}, USD : {dollor}\n")
-                        trade_val = trade(ACCESS_TOKEN, arr['ovrs_excg_cd'], arr['ovrs_pdno'], arr['now_pric2'])
-                        w.write(f"{trade_val['msg1']}\n")
-                        data = {
-                            'object_type': 'text',
-                            'text': f"({trade_val['msg1']}\nUSD : {format(dollor,',')}\n{arr['ovrs_pdno']}({arr['ovrs_excg_cd']})\n현재가 : {arr['now_pric2']}\nKOR : {format(round(kor, 2),',')}",
-                            'link': {
-                                'web_url': 'https://developers.kakao.com',
-                                'mobile_web_url': 'https://developers.kakao.com'
-                            },
-                        }
+                if(price<=200):
+                    w.write(f"({time.strftime('%Y-%m-%d %H:%M:%S')}) {arr['ovrs_pdno']}({arr['ovrs_excg_cd']}), 현재가 : {arr['now_pric2']}, 평균가 : {arr['pchs_avg_pric']}, 차액(현재가-평균가) : {round((float(arr['now_pric2']) - float(arr['pchs_avg_pric'])), 2)}, 보유수량 : {int(arr['ovrs_cblc_qty'])}, KOR : {round(kor, 2)}, USD : {dollor}\n")
+                    trade_val = trade(ACCESS_TOKEN, arr['ovrs_excg_cd'], arr['ovrs_pdno'], arr['now_pric2'])
+                    w.write(f"{trade_val['msg1']}\n")
+                    data = {
+                        'object_type': 'text',
+                        'text': f"({trade_val['msg1']}\nUSD : {format(dollor,',')}\n{arr['ovrs_pdno']}({arr['ovrs_excg_cd']})\n평가금액 : {format(int(arr['ovrs_cblc_qty'])*round(kor, 2),',')}\n현재가 : {arr['now_pric2']}\nKOR : {format(round(kor, 2),',')}",
+                        'link': {
+                            'web_url': 'https://developers.kakao.com',
+                            'mobile_web_url': 'https://developers.kakao.com'
+                        },
+                    }
 
-                        data = {'template_object': json.dumps(data)}
-                        response = requests.post(kakao_url, headers=headers, data=data)
-                        response.status_code
+                    data = {'template_object': json.dumps(data)}
+                    response = requests.post(kakao_url, headers=headers, data=data)
+                    response.status_code
 
     except Exception as e:
         print(e)
-        #f = open("/home/project/stock/token.txt", 'w', encoding='utf-8')
-        f = open("./token.txt", 'w', encoding='utf-8')
+        f = open("/home/project/stock/token.txt", 'w', encoding='utf-8')
+        #f = open("./token.txt", 'w', encoding='utf-8')
         f.write(token())
 
 def token():
