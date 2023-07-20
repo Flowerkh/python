96e53bbbbfe6b38e36a4ab3f73299ccc283e56df
KyungHa Kim <32595112+Flowerkh@users.noreply.github.com>
2023-07-20 11:03:39 +0900

    Update queue.py

--- Lamda/queue.py
+++ Lamda/queue.py
@@ -7,15 +7,16 @@ from datetime import datetime, timedelta
 
 
 def lambda_handler(event, context):
+
     ktc_time = datetime.now(timezone('Asia/Seoul'))
-    # 이벤트 시작 시간 (정책별 상이)
+    # 이벤트 시작 시간
     start_time = ktc_time.replace(hour=10, minute=0, second=0, microsecond=0)
-    # 이벤트 종료 시간 (정책별 상이)
+    # 이벤트 종료 시간
     end_time = ktc_time.replace(hour=15, minute=1, second=0, microsecond=0)
     ktc_today = ktc_time.strftime("%Y%m%d")
-    
+
     # memcache 주소
-    memcache_addr = 'client-count-memcache.kmioik.cfg.apn2.cache.amazonaws.com:11211'
+    memcache_addr = 'receptionprocess.kmioik.0001.apn2.cache.amazonaws.com:11211'
 
     # 당일 memcache 초기화
     if event['queryStringParameters']['selectLang'] == 'clear':
@@ -67,18 +68,19 @@ def lambda_handler(event, context):
                 num = 1
                 memcache_client.set(ktc_today, num)
             else:
-                # 선착순 인원 초과시 (선착순 인원 정책별 상이)
+                # 선착순 인원 초과시 (선착순 인원)
                 if int(num) >= 10:
+                    #body_dict['num'] = num
                     body_dict['isSuc'] = 'N'
                     body_dict['error'] = 'over daily limit'
                 else:
                     # 당일 신청 인원 카운트
                     num = int(num) + 1
                     memcache_client.set(ktc_today, num)
-
                     body_dict['num'] = int(num)
         else:
             # 중복 신청일 경우
+            #body_dict['count'] = num
             body_dict['isSuc'] = 'N'
             body_dict['error'] = 'already submit'
 
@@ -95,84 +97,3 @@ def lambda_handler(event, context):
                 'error': 'not submit time'
             })
         }
-        
-# OLD        
-# import json
-# import os
-
-# from pymemcache.client.base import Client
-# from pytz import timezone
-# from datetime import datetime, timedelta
-
-
-# def lambda_handler(event, context):
-#     ktc_time = datetime.now(timezone('Asia/Seoul'))
-#     start_time = ktc_time.replace(hour=10, minute=0, second=0, microsecond=0)
-#     end_time = ktc_time.replace(hour=15, minute=1, second=0, microsecond=0)
-#     ktc_today = ktc_time.strftime("%Y%m%d")
-    
-#     if event['queryStringParameters']['selectLang'] == 'clear':
-#         memcache_client = Client('client-count-memcache.kmioik.cfg.apn2.cache.amazonaws.com:11211')
-#         memcache_client.set(ktc_today, 0)
-        
-#         return {
-#             'statusCode': 200,
-#             'body': json.dumps({'msg': 'clear'})
-#         }
-#     elif event['queryStringParameters']['selectLang'] == 'flushall':
-#         memcache_client = Client('client-count-memcache.kmioik.cfg.apn2.cache.amazonaws.com:11211')
-#         memcache_client.flush_all()
-        
-#         return {
-#             'statusCode': 200,
-#             'body': json.dumps({'msg': 'flush all memcache'})
-#         }
-#     elif start_time < ktc_time and ktc_time < end_time:
-#         user_real_id = event['queryStringParameters']['realID']
-#         body_dict = {
-#             'gToken': event['queryStringParameters']['gToken'],
-#             'isSuc': 'Y'
-#         }
-        
-#         # connect memcache
-#         memcache_client = Client('client-count-memcache.kmioik.cfg.apn2.cache.amazonaws.com:11211')
-        
-#         # Get daily count
-#         num = memcache_client.get(ktc_today)
-#         # Check user submit
-#         user_exist = memcache_client.get(user_real_id)
-        
-#         # Check submit done
-#         if user_exist is None:
-#             memcache_client.set(user_real_id, 1)
-            
-#             # Check daily limit done
-#             if num is None:
-#                 num = 1
-#                 memcache_client.set(ktc_today, num)
-#             else:
-#                 if int(num) >= 10:
-#                     body_dict['isSuc'] = 'N'
-#                     body_dict['error'] = 'over daily limit'
-#                 else:
-#                     num = int(num) + 1
-#                     memcache_client.set(ktc_today, num)
-                    
-#                     body_dict['num'] = int(num)
-#         else:
-#             body_dict['isSuc'] = 'N'
-#             body_dict['error'] = 'already submit'
-        
-#         return {
-#             'statusCode': 200,
-#             'body': json.dumps(body_dict)
-#         }
-#     else:
-#         return {
-#             'statusCode': 200,
-#             'body': json.dumps({
-#                 'isSuc': 'N',
-#                 'error': 'not submit time'
-#             })
-#         }
-    
