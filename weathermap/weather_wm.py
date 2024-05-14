4f2a5f3cbf32001fb82e24c5512b22e3f3b0deac
kimkyungha <cdffee1@macrogen.com>
2024-05-14 13:10:59 +0900

    20240514 kakao 함수 적용

--- weathermap/weather_wm.py
+++ weathermap/weather_wm.py
@@ -1,32 +1,9 @@
 import requests
-import json
 import re
 from datetime import datetime,timedelta
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from kakao import send
-
-#카카오 발송
-def kakao(msg) :
-    with open("/var/project/python/kakao/kakao_code.json", "r") as kakao:
-    #with open("../kakao/kakao_code.json", "r") as kakao:
-        kaka_tks = json.load(kakao)
-    kakao_url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
-    headers = {"Authorization": "Bearer " + kaka_tks["access_token"]}
-
-    data = {
-        'object_type': 'text',
-        'text': msg,
-        'link': {
-            'web_url': 'https://developers.kakao.com',
-            'mobile_web_url': 'https://developers.kakao.com'
-        },
-    }
-
-    # 카카오톡 메세지 전송
-    data = {'template_object': json.dumps(data)}
-    result = requests.post(kakao_url, headers=headers, data=data)
-    print(result)
 
 def file(file_name) :
     w_msg = re.sub('(#.+)|(=)|(\$.+)|#', '', response.text)
