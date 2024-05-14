cd5b94bec2fd0d9bc9c2d60dbc9bf4f3af1bcfa9
kimkyungha <cdffee1@macrogen.com>
2024-05-14 10:32:44 +0900

    20240514 날씨 반영

--- weathermap/weather_wm.py
+++ weathermap/weather_wm.py
@@ -24,21 +24,50 @@ def kakao(msg) :
     result = requests.post(kakao_url, headers=headers, data=data)
     print(result)
 
+def file(file_name) :
+    w_msg = re.sub('(#.+)|(=)|(\$.+)|#', '', response.text)
+    w_msg = re.sub(' +', ' ', w_msg)
+    w_msg = re.sub('"', '', w_msg)
+    with open(file_name, 'w') as f:
+        f.write(w_msg)
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        lines = [line.rstrip('\n') for line in lines]
+    return lines
+
+#강수유무
+def PRE(value) :
+    pre = {"WB09":"비", "WB11":"비/눈", "WB13":"눈/비", "WB12":"눈"}.get(value, "무")
+    return pre
+
 """
 stnld(지역) : 108(전체), 109(수도권), 133(대전), 156(광주), 159(부산), 154(제주)
+reg(지역) : 11B10101(서울), 11B20601(수원), 11C20401(대전), 11F20501(광주), 11H10701(대구), 11H20201(부산)
+f_reg(지역) : 11B00000(수도권), 
 """
-numOfRows = 10
-dataType = "JSON"
 stnId = "109"
+reg = "11B10101"
+f_reg = "11B00000"
 authKey = "qEFBNuDQS0uBQTbg0JtL0g"
 
-#https://apihub.kma.go.kr/api/typ01/url/fct_afs_ws.php?stn=109&mode=0&disp=1&help=0&authKey=qEFBNuDQS0uBQTbg0JtL0g
-url = f"https://apihub.kma.go.kr/api/typ02/openApi/VilageFcstMsgService/getWthrSituation?pageNo=1&numOfRows={numOfRows}&dataType={dataType}&stnId={stnId}&authKey={authKey}"
-response = requests.get(url)
-if response.status_code == 200 :
-    result = json.loads(response.text)
-    msg = re.sub(' +', ' ', result['response']['body']['items']['item'][0]['wfSv1']+"\n\n※ 특이사항 : "+result['response']['body']['items']['item'][0]['wr'].replace('o ', ''))
-    #print(msg.replace(' 곳', ''))
-    kakao(msg.replace(' 곳', ''))
-else :
+w_url = f"https://apihub.kma.go.kr/api/typ01/url/fct_afs_wc.php?reg={reg}&mode=0&disp=1&help=0&authKey={authKey}"
+response = requests.get(w_url)
+if response.status_code == 200:
+    w_lines = file('weather_text')
+    min_tmp = w_lines[2].split(',')[6]
+    max_tmp = w_lines[2].split(',')[7]
+
+    wf_url = f"https://apihub.kma.go.kr/api/typ01/url/fct_afs_wl.php?reg={f_reg}&mode=0&disp=1&help=0&authKey={authKey}"
+    response = requests.get(wf_url)
+    if response.status_code == 200:
+        f_lines = file('fweather_text')
+        pre = PRE(f_lines[2].split(',')[7]) #강수유무
+        rn_st = f_lines[2].split(',')[10] #강수확률
+        wf = f_lines[2].split(',')[9] #하늘상태
+
+        msg = f"수도권({wf} / {pre})\n기온 : {min_tmp}° / {max_tmp}°\n강수 확률 : {rn_st}%"
+        kakao(msg)
+    else:
+        print("FAIL Code : 20002")
+else:
     result = "FAIL Code : 20001"
