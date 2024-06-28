2a99a465b09ff75937a928df6080efdc42790582
kimkyungha <cdffee1@macrogen.com>
2024-06-28 09:59:54 +0900

    20240626 환율 API 갱신

--- project/stock_macro/stock.py
+++ project/stock_macro/stock.py
@@ -15,64 +15,74 @@ NAS : 나스닥
 """
 def main():
     time = datetime.today().strftime("%H:%M")
+    key = "EMJld60MM2JVYvmBkwGSp1fDe1HEIDrg"
+    url = f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={key}&searchdate=20240627&data=AP01"
+    result = requests.get(url)
+    if result.status_code == 200:
+        json_data = json.loads(result.text)
+        for jd in json_data:
+            if jd['cur_unit'] == 'USD':
+                USD = jd['deal_bas_r']
+        f = open("/var/project/python/project/stock_macro/token.txt", 'r', encoding='utf-8')
+        # f = open("./token.txt", 'r', encoding='utf-8')
+        dollor = USD
 
-    f = open("/var/project/python/project/stock_macro/token.txt", 'r', encoding='utf-8')
-    #f = open("./token.txt", 'r', encoding='utf-8')
-    dollor = 1390
+        try:
+            line = f.readline()
+            ACCESS_TOKEN = line
 
-    try:
-        line = f.readline()
-        ACCESS_TOKEN = line
+            # 종목 금액 조회
+            QQQY = usd_search(ACCESS_TOKEN, "NAS", 'QQQY')  # 나스닥 QQQY
+            NVDA = usd_search(ACCESS_TOKEN, "NAS", 'NVDA')  # 나스닥 엔비디아
+            SPLG = usd_search(ACCESS_TOKEN, "AMS", 'SPLG')  # 뉴욕 SPLG
 
-        # 종목 금액 조회
-        QQQY = usd_search(ACCESS_TOKEN, "NAS", 'QQQY')  # 나스닥 QQQY
-        NVDA = usd_search(ACCESS_TOKEN, "NAS", 'NVDA')  # 나스닥 엔비디아
-        SPLG = usd_search(ACCESS_TOKEN, "AMS", 'SPLG')  # 뉴욕 SPLG
+            # 종목 환전 금액 (달러 > 원화)
+            qqqy_price = float(QQQY['last']) * dollor
+            splg_price = float(SPLG['last']) * dollor
+            nvda_price = float(NVDA['last']) * dollor
 
-        # 종목 환전 금액 (달러 > 원화)
-        qqqy_price = float(QQQY['last']) * dollor
-        splg_price = float(SPLG['last']) * dollor
-        nvda_price = float(NVDA['last']) * dollor
+            msg = f"환율 : {dollor}" \
+                  f"\nNVDA : {format(round(nvda_price, 2), ',')} 원(${format(round(float(NVDA['last']), 2), ',')})" \
+                  f"\nQQQY : {format(round(qqqy_price, 2), ',')} 원(${format(round(float(QQQY['last']), 2), ',')})" \
+                  f"\nSPLG : {format(round(splg_price, 2), ',')} 원(${format(round(float(SPLG['last']), 2), ',')})"
 
-        msg = f"환율 : {dollor}" \
-              f"\nNVDA : {format(round(nvda_price, 2), ',')} 원(${format(round(float(NVDA['last']), 2), ',')})" \
-              f"\nQQQY : {format(round(qqqy_price, 2), ',')} 원(${format(round(float(QQQY['last']), 2), ',')})" \
-              f"\nSPLG : {format(round(splg_price, 2), ',')} 원(${format(round(float(SPLG['last']), 2), ',')})"
+            # 현재 해외 잔고
+            info_result = info(ACCESS_TOKEN, "NASD")
+            info_dic = {}
+            msg = msg + "\n★★★수익률★★★"
+            for info_data in info_result:
+                info_dic[info_data['ovrs_pdno']] = info_data['ord_psbl_qty']
+                msg = msg + f"\n{info_data['ovrs_pdno']}({info_data['ord_psbl_qty']}) : {info_data['evlu_pfls_rt']}%"
 
-        # 현재 해외 잔고
-        info_result = info(ACCESS_TOKEN, "NASD")
-        info_dic = {}
-        msg = msg + "\n★★★수익률★★★"
-        for info_data in info_result:
-            info_dic[info_data['ovrs_pdno']] = info_data['ord_psbl_qty']
-            msg = msg + f"\n{info_data['ovrs_pdno']}({info_data['ord_psbl_qty']}) : {info_data['evlu_pfls_rt']}%"
+            # if int(info_dic['NVDA']) < (int(info_dic['QQQY'])+int(info_dic['SPLG']))*2:
+            #     #NVDA 구매
+            #     price = round(float(NVDA['last']), 2)
+            #     result = trade(ACCESS_TOKEN, 'NASD', 'NVDA', str(price))
+            # el
+            if int(info_dic['QQQY']) > int(info_dic['SPLG']):
+                # SPLG 구매
+                if splg_price <= 100000:
+                    price = round(float(SPLG['last']), 2)
+                    result = trade(ACCESS_TOKEN, 'AMEX', 'SPLG', str(price))
+            else:
+                # QQQY 구매
+                if qqqy_price <= 21000:
+                    price = round(float(QQQY['last']), 2)
+                    result = trade(ACCESS_TOKEN, 'NASD', 'QQQY', str(price))
 
-        # if int(info_dic['NVDA']) < (int(info_dic['QQQY'])+int(info_dic['SPLG']))*2:
-        #     #NVDA 구매
-        #     price = round(float(NVDA['last']), 2)
-        #     result = trade(ACCESS_TOKEN, 'NASD', 'NVDA', str(price))
-        # el
-        if int(info_dic['QQQY']) > int(info_dic['SPLG']):
-            # SPLG 구매
-            if splg_price <= 100000:
-                price = round(float(SPLG['last']), 2)
-                result = trade(ACCESS_TOKEN, 'AMEX', 'SPLG', str(price))
-        else:
-            # QQQY 구매
-            if qqqy_price <= 21000:
-                price = round(float(QQQY['last']), 2)
-                result = trade(ACCESS_TOKEN, 'NASD', 'QQQY', str(price))
+            print(msg)
+            # 카카오 메신저 발송
+            # if datetime.now().strftime('%H:%M') >= "04:10":
+            send.kakao(msg)
 
-        print(msg)
-        # 카카오 메신저 발송
-        # if datetime.now().strftime('%H:%M') >= "04:10":
-        send.kakao(msg)
+        except Exception as e:
+            print(f'Error [%s]' % (str(e)))
+            f = open("/var/project/python/project/stock_macro/token.txt", 'w', encoding='utf-8')
+            # f = open("./token.txt", 'w', encoding='utf-8')
+            f.write(token())  # 토큰 없으면 생성
 
-    except Exception as e:
-        print(f'Error [%s]' % (str(e)))
-        f = open("/var/project/python/project/stock_macro/token.txt", 'w', encoding='utf-8')
-        #f = open("./token.txt", 'w', encoding='utf-8')
-        f.write(token())  # 토큰 없으면 생성
+    else:
+        print("error03 : "+result.status_code)
 
 def token():
     URL = f"{Sync_API.URL_BASE}{Sync_API.PATH}"
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-from datetime import datetime
+import sys, os
+import requests
+import json
 
-if datetime.now().strftime('%H:%M') >= "21:30":
-    print(datetime.now().strftime('%H:%M'))
+key = "EMJld60MM2JVYvmBkwGSp1fDe1HEIDrg"
+url = f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={key}&searchdate=20240627&data=AP01"
+result = requests.get(url)
+if result.status_code == 200:
+    json_data = json.loads(result.text)
+    for jd in json_data:
+        if jd['cur_unit'] == 'USD':
+            USD = jd['deal_bas_r']
