08113ae53e0aff0310d4d055e42eeb086a0152c6
kimkyungha <cdffee1@macrogen.com>
2024-07-02 09:05:00 +0900

    20240702 기상일보 추가

--- weathermap/weather_wm.py
+++ weathermap/weather_wm.py
@@ -49,12 +49,12 @@ if response.status_code == 200:
         PCP_pm = "(0mm)" if informations['1400']['PCP'] == '강수없음' else " ("+informations['1400']['PCP']+")"
 
         msg = f"오늘의 날씨 (오전/오후)" \
-              f"\n기상 : {pty_code[informations['0600']['PTY']]} / {pty_code[informations['1400']['PTY']]}" \
-              f"\n구름 : {sky_code[informations['0600']['SKY']]} / {sky_code[informations['1400']['SKY']]}" \
+              f"\n기상 : {pty_code[informations['0600']['PTY']]}({sky_code[informations['0600']['SKY']]}) / {pty_code[informations['1400']['PTY']]}({sky_code[informations['1400']['SKY']]})" \
               f"\n기온 : {informations['0600']['TMP']}°C / {informations['1400']['TMP']}°C" \
               f"\n습도 : {informations['0600']['REH']}% / {informations['1400']['REH']}%" \
               f"\n강수 : {informations['0600']['POP']}%{PCP_am} / {informations['1400']['POP']}%{PCP_pm}"\
               f"\n풍속 : {informations['0600']['WSD']} ㎧ / {informations['1400']['WSD']} ㎧"
+
         #print(msg)
         send.kakao(msg)
 else:
