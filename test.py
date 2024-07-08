0b83b2e47b3c1effbf4bbbf48151d6189f83f39c
kimkyungha <cdffee1@macrogen.com>
2024-07-09 08:53:28 +0900

    20240709 환율 가져오기

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
-url = f"http://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=EMJld60MM2JVYvmBkwGSp1fDe1HEIDrg&searchdate=20240708&data=AP01"
+key = "EMJld60MM2JVYvmBkwGSp1fDe1HEIDrg"
+url = f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={key}&searchdate=20240627&data=AP01"
 result = requests.get(url)
 if result.status_code == 200:
     json_data = json.loads(result.text)
     for jd in json_data:
         if jd['cur_unit'] == 'USD':
-            USD = jd['deal_bas_r'].replace(",", "")
+            USD = jd['deal_bas_r'].replace(",","")
 
 print(USD)
